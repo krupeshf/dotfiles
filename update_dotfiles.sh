d7bfa6c782c82c9cb613381a13826a0413f25336
Krupesh <krupesh.faldu@clover.com>
2021-10-12 16:13:41 -0400

    Update dotfile with automated script

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -4,9 +4,13 @@ set -o xtrace
 
 homeDirectory=/Users/$USER
 
-cp $homeDirectory/.bash_profile $homeDirectory/.vimrc $homeDirectory/.gitignore_global .
+cp $homeDirectory/.bash_profile \
+  $homeDirectory/.vimrc \
+  $homeDirectory/.gitignore_global \
+  $homeDirectory/.zshrc.pre-oh-my-zsh \
+  .
 
 mkdir -p .aws
 cp $homeDirectory/.aws/config .aws/config
 
-git add . && git commit -m "Updating dotfile with automated script" && git push
+git add . && git commit -m "Update dotfile with automated script" && git push
